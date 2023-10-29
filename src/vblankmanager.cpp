df9e9036cb95d1d7119c1c916002e8fe09dd7d1b
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2023-10-28 21:04:12 -0400

    attempt to fix some stuff x 3

--- src/vblankmanager.cpp
+++ src/vblankmanager.cpp
@@ -211,7 +211,7 @@ void __attribute__((optimize("-fno-unsafe-math-optimizations") )) vblankThreadRu
 		lastOffset = offset;
 #endif
 		uint64_t targetPoint;
-		if ((offset/(2*sleep_cycle))<1'200'000l && prev_evaluation != (offset/(2*sleep_cycle)))
+		if ((offset/(2*sleep_cycle))<1'200'000l && prev_evaluation > (offset/(2*sleep_cycle)))
 		{
 			std::cout << "sleep_cycle=" << sleep_cycle << "\n"
 			<< "\n"
