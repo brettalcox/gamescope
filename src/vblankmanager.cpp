5b3dc0b798571e1b72d10fbe6407a64aa68aa2d2
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2023-10-31 09:12:19 -0400

    remove a cout

--- src/vblankmanager.cpp
+++ src/vblankmanager.cpp
@@ -292,7 +292,7 @@ void __attribute__((optimize("-fno-unsafe-math-optimizations") )) vblankThreadRu
 			slept=false;
 			targetPoint = vblank_next_target( static_cast<uint64_t>(llroundl(offset*vblank_adj_factor)) );
 			prev_evaluation=( static_cast<uint64_t>( llroundl( static_cast<long double>(offset*refresh) / static_cast<long double>(2*sleep_cycle*g_nOutputRefresh))));
-			std::cout << "exited busy wait loop\n";
+			//std::cout << "exited busy wait loop\n";
 		}
 		else
 		{
