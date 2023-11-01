7e806d7408be352bcb8291b44f1f6278e982ddf3
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2023-10-31 20:00:57 -0400

    vblankmanager: more adjustments 2

--- src/vblankmanager.cpp
+++ src/vblankmanager.cpp
@@ -260,8 +260,8 @@ void __attribute__((optimize("-fno-unsafe-math-optimizations") )) vblankThreadRu
 			
 				// If we need to offset for our draw more than half of our vblank, something is very wrong.
 				// Clamp our max time to half of the vblank if we can.
-				rollingMaxDrawTime =  (nsecInterval + 6*rollingMaxDrawTime + centered_mean)/8;
-				std::cout << "(nsecInterval + 6*rollingMaxDrawTime + centered_mean)/8 = " << (nsecInterval + 6*rollingMaxDrawTime + centered_mean)/8 << "\n";
+				rollingMaxDrawTime =  (nsecInterval + 6*rollingMaxDrawTime + centered_mean)/7;
+				std::cout << "(nsecInterval + 6*rollingMaxDrawTime + centered_mean)/8 = " << (nsecInterval + 6*rollingMaxDrawTime + centered_mean)/7 << "\n";
 				if (sleep_cycle > 1)
 				{
 					g_uRollingMaxDrawTime = rollingMaxDrawTime;
