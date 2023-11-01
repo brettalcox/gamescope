204eab1878ac674ccc25ad1604bab50757299c46
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2023-10-31 22:31:56 -0400

    vblankmanager: fix misspelling

--- src/vblankmanager.cpp
+++ src/vblankmanager.cpp
@@ -274,7 +274,7 @@ void __attribute__((optimize("-fno-unsafe-math-optimizations") )) vblankThreadRu
 				}
 				else
 				{
-					rollingMaxDrawTime = std::clamp(centered_mean - std::abs(static_cast<int64_t>(lastDrawTime) - static_last<int64_t>(drawTime)), ( ( alpha * rollingMaxDrawTime ) + ( range - alpha ) * drawTime ) / range, centered_mean + std::abs(static_cast<int64_t>(lastDrawTime) - static_last<int64_t>(drawTime)));
+					rollingMaxDrawTime = std::clamp(centered_mean - std::abs(static_cast<int64_t>(lastDrawTime) - static_cast<int64_t>(drawTime)), ( ( alpha * rollingMaxDrawTime ) + ( range - alpha ) * drawTime ) / range, centered_mean + std::abs(static_cast<int64_t>(lastDrawTime) - static_cast<int64_t>(drawTime)));
 				}
 				offset = rollingMaxDrawTime + redZone;
 				
