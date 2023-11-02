51f18962804c37adbbba1672627b7206524c351e
sharkautarch <128002472+sharkautarch@users.noreply.github.com>
2023-11-02 13:56:24 -0400

    another adjustment 2

--- src/vblankmanager.cpp
+++ src/vblankmanager.cpp
@@ -420,7 +420,7 @@ void __attribute__((optimize("-fno-unsafe-math-optimizations"), hot )) vblankThr
 		if (!slept || skipped_sleep_after_vblank >= 3 )
 		{
 			skipped_sleep_after_vblank=0;
-			sleep_for_nanos( offset + static_cast<uint64_t> (1'000'000*std::sqrt(vblank_adj_factor) );
+			sleep_for_nanos( offset + static_cast<uint64_t> ( llroundl(1'000'000.0*std::sqrt(vblank_adj_factor))) );
 		}
 		else
 		{
@@ -469,7 +469,7 @@ void __attribute__((optimize("-fno-unsafe-math-optimizations"), hot )) vblankThr
 					break;
 				}
 			}
-			while ( static_cast<uint64_t> (res) <  offset + static_cast<uint64_t> (1'000'000*std::sqrt(vblank_adj_factor)));		
+			while ( static_cast<uint64_t> (res) <  offset + static_cast<uint64_t> ( llroundl(1'000'000.0*std::sqrt(vblank_adj_factor))));		
 			skipped_sleep_after_vblank++;
 		}
 		sleep_cycle=0;
